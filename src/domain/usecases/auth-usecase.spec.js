796b90c050f4523152f45e7179328c2dfa326d2c
= <=>
2020-05-07 02:35:36 -0300

    ensure AuthUseCase throws MissingParamError if no email is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,3 +1,5 @@
+const { MissingParamError } = require('../../utils/errors')
+
 class AuthUseCase {
   async auth (email) {
     if (!email) throw new Error()
@@ -8,6 +10,6 @@ describe('Auth UseCase', () => {
   test('Should return null if no email is provided ', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
